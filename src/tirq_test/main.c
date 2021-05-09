cbe7434ebdb69aea6e6a01cc33248c4f41eed6bb
cccc <cuiwei_cv@163.com>
2021-05-09 15:24:43 +0800

    timer3 interupt fail

--- src/tirq_test/main.c
+++ src/tirq_test/main.c
@@ -1,9 +1,12 @@
 
 #include <xbr820.h>
 
-extern void timer_init(void);
+extern void timer_init(unsigned char chan, unsigned int val);
 
 extern volatile uint32_t tm_count;
+extern volatile uint32_t tm_count1;
+extern volatile uint32_t tm_count2;
+extern volatile uint32_t tm_count3;
 
 void delay(unsigned int val)
 {
@@ -13,6 +16,34 @@ void delay(unsigned int val)
 		//
 	}
 }
+
+void delay1(unsigned int val)
+{
+	tm_count1 = 0;
+	while(tm_count1 < val)
+	{
+		//
+	}
+}
+
+void delay2(unsigned int val)
+{
+	tm_count2 = 0;
+	while(tm_count2 < val)
+	{
+		//
+	}
+}
+
+void delay3(unsigned int val)
+{
+	tm_count3 = 0;
+	while(tm_count3 < val)
+	{
+		//
+	}
+}
+
 int main() {
     /* get interrupt level from info */
     CLIC->CLICCFG = (((CLIC->CLICINFO & CLIC_INFO_CLICINTCTLBITS_Msk) >> CLIC_INFO_CLICINTCTLBITS_Pos) << CLIC_CLICCFG_NLBIT_Pos);
@@ -25,12 +56,20 @@ int main() {
         CLIC->CLICINT[i].ATTR = 3; /* use positive egde vector interrupt */
     }
     __enable_excp_irq();
+	
+	
 
-	timer_init();
+	timer_init(0, SYSTEM_CLOCK / 1000);	//1ms
+	timer_init(1, SYSTEM_CLOCK / 100);	//10ms
+	timer_init(2, SYSTEM_CLOCK / 10);	//100ms
+	timer_init(3, SYSTEM_CLOCK / 100);	//10ms
 
 	while(1)
 	{
-		delay(100);
+		delay(1000);
+		delay1(100);
+		delay2(10);
+		delay3(100);
 	}
 	return 0;
 }
--- src/tirq_test/timer.c
+++ src/tirq_test/timer.c
@@ -7,16 +7,43 @@
 #define TIMCNT				((ro32_t *)(XBR820_STC_BASE + 0x40))
 
 #define TMEN(i)				(0x10 << (i))
-#define TMCLR(i)				(0x100 << (i))
+#define TMCLR(i)			(0x100 << (i))
 
 volatile uint32_t tm_count;
+volatile uint32_t tm_count1;
+volatile uint32_t tm_count2;
+volatile uint32_t tm_count3;
 
-void timer_init(void) {
-	tm_count = 0;
-	//TIMINIT[0] = SYSTEM_CLOCK / 10;	// 100ms
-	TIMINIT[0] = SYSTEM_CLOCK / 1000;	// 1ms
-	reg32_set(STCCTL, TMEN(0));
-	csi_vic_enable_irq(TIM0_IRQn);
+void timer_init(unsigned char chan, unsigned int val) {
+	switch (chan)
+	{
+		case 0:
+			TIMINIT[0] = val;
+			reg32_set(STCCTL, TMEN(0));
+			csi_vic_enable_irq(TIM0_IRQn);		
+		break;
+		
+		case 1:
+			TIMINIT[1] = val;
+			reg32_set(STCCTL, TMEN(1));
+			csi_vic_enable_irq(TIM1_IRQn);		
+		break;
+		
+		case 2:
+			TIMINIT[2] = val;
+			reg32_set(STCCTL, TMEN(2));
+			csi_vic_enable_irq(TIM2_IRQn);			
+		break;
+		
+		case 3:
+			TIMINIT[3] = val;
+			reg32_set(STCCTL, TMEN(3));
+			csi_vic_enable_irq(TIM3_IRQn);			
+		break;
+		
+		default:
+		break;
+	}
 }
 
 void handle_irq(uint32_t vec) {
@@ -24,4 +51,16 @@ void handle_irq(uint32_t vec) {
 		tm_count++;
 		reg32_set(STCCTL, TMCLR(0));
 	}
+	if (vec == TIM1_IRQn) {
+		tm_count1++;
+		reg32_set(STCCTL, TMCLR(1));
+	}
+	if (vec == TIM2_IRQn) {
+		tm_count2++;
+		reg32_set(STCCTL, TMCLR(2));
+	}
+	if (vec == TIM3_IRQn) {
+		tm_count3++;
+		reg32_set(STCCTL, TMCLR(3));
+	}	
 }
