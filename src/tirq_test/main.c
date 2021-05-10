360ebd5e23fe3ed742f4663e9f26070dfcca71de
cccc <cuiwei_cv@163.com>
2021-05-10 11:19:05 +0800

    update timer driver

--- src/tirq_test/main.c
+++ src/tirq_test/main.c
@@ -1,7 +1,7 @@
 
 #include <xbr820.h>
 
-extern void timer_init(unsigned int val);
+extern void timer_init(unsigned char chan, unsigned int val);
 extern volatile uint32_t tm_count;
 
 void delay(unsigned int val)
@@ -16,7 +16,7 @@ void delay(unsigned int val)
 int main() {
     __enable_excp_irq();
 
-	timer_init(SYSTEM_CLOCK / 1000); // 1ms
+	timer_init(0, SYSTEM_CLOCK / 1000); // 1ms
 
 	while(1)
 	{
