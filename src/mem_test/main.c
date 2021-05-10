11bffaf8358a3ecf0392e40051a066627072f649
cccc <cuiwei_cv@163.com>
2021-05-10 11:57:19 +0800

    add mem test case, but download flash fail

---
+++ src/mem_test/main.c
@@ -0,0 +1,12 @@
+#include "xbr820.h"
+
+typedef	volatile uint32_t*	TEST_PTR;
+TEST_PTR ptr = (TEST_PTR)(XBR820_SRAM_BASE + XBR820_SRAM_SIZE/2);
+
+int main() {
+	for (int i = 0; i < 256; i++) {
+		ptr[i] = i;
+	}
+	while(1);
+	return 0;
+}
