42680ac6ad2774495da6a06ebb3640e8d524e277
cccc <cuiwei_cv@163.com>
2021-05-12 21:37:33 +0800

    first working in flash example for uart1

--- src/uart1_tx/cdkkilll.bat
+++
@@ -1,6 +0,0 @@
-del *.o /s
-del *.d /s
-del *.ihex /s
-del __rt_entry.S /s
-
-exit
--- src/uart1_tx/cdkws.mk
+++ src/uart1_tx/cdkws.mk
@@ -1,9 +1,14 @@
-.PHONY: clean All Project_Title Project_Build
+.PHONY: clean All Project_Title Project_PreBuild Project_Build
 
-All: Project_Title Project_Build
+All: Project_Title Project_PreBuild Project_Build
 
 Project_Title:
 	@echo "----------Building project:[ uart1_tx - BuildSet ]----------"
+
+Project_PreBuild:
+	@echo Executing Pre Build commands ...
+	@export CDKPath="D:/C-Sky/CDK" CDK_VERSION="V2.8.7" ProjectPath="D:/cccc2020/CODE/e902/src/uart1_tx/" && riscv64-unknown-elf-gcc -I. -I ../tools -E -P ../tools/memory.ld.c -o Obj/memory.ld
+	@echo Done
 
 Project_Build:
 	@make -r -f uart1_tx.mk -j 8 -C  ./ 
