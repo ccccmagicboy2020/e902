3d838a9106c4475d69030abf5c501312e54354ac
cccc <cuiwei_cv@163.com>
2021-05-12 20:33:48 +0800

    del useless files

--- src/mem_test/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BOOT_TEST_CONFIG_H__
-#define __BOOT_TEST_CONFIG_H__
-#define CONFIG_ARCH_RV32 1
-#define CONFIG_CPU_E902 1
-#define CONFIG_ROM
-#define __NO_ICACHE
-//#define CONFIG_BSS_INIT
-#endif
--- src/mem_test2/config.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MEM_TEST2_CONFIG_H__
-#define __MEM_TEST2_CONFIG_H__
-#define CONFIG_ARCH_RV32 1
-#define CONFIG_CPU_E902 1
-#define CONFIG_ROM
-//#define __NO_ICACHE
-#endif
